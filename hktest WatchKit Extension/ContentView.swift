57e2e47c4577ea5776f7ae002110dd5bd3a45c8c
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2021-12-31 07:22:26 +0000

    Initial Commit

---
+++ hktest WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hktest WatchKit Extension
+//
+//  Created by Óðinn Dagur Bjarnason on 31.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hktest WatchKit Extension/hktestApp.swift
@@ -0,0 +1,19 @@
+//
+//  hktestApp.swift
+//  hktest WatchKit Extension
+//
+//  Created by Óðinn Dagur Bjarnason on 31.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct hktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
